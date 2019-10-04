3fe85afee56c9abe0e336808587acf3b5f617922
unknown <robertrey1234@gmail.com>
2019-10-04 11:42:22 -0500

    Crud for projects complete.

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -3,4 +3,63 @@ const Projects = require("../data/helpers/projectModel");
 const Actions = require("../data/helpers/actionModel");
 const router = express.Router();
 
+//Endpoints
+
+router.get("/", (req, res) => {
+  Projects.get()
+    .then(projects => {
+      res.status(201).json(projects);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Server could not GET projects" });
+    });
+});
+
+router.post("/", (req, res) => {
+  const { name, description } = req.body;
+
+  if (name || description) {
+    Projects.insert()
+      .then(project => {
+        res.status(201).json(project);
+      })
+      .catch(() => {
+        res.status(500).json({ error: "Server could not POST" });
+      });
+  } else {
+    res.status(404).json({ message: "Please provide name and description" });
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params.id;
+
+  if (!id) {
+    res.status(404).json({ error: "That project doesn't even exist" });
+  } else {
+    Projects.remove(id)
+      .then(() => {
+        res.status(201).json();
+      })
+      .catch(500)
+      .json({ error: "Server could not delete project." });
+  }
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params.id;
+  Projects.update(id, req.body)
+    .then(project => {
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({ message: "Project does not exist." });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Server could not update." });
+    });
+});
+
 module.exports = router;
